6f4ed623845edcde680456e4b3d4ce33132ac859
cpuQ <42661264+cpuQ@users.noreply.github.com>
2023-07-19 14:02:26 +0900

    added press key to close so you know if it did it right lolxd

--- the actual skin/!ultrainstinct/!ultrainstinct.bat
+++ the actual skin/!ultrainstinct/!ultrainstinct.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal
 
-rem Set the paths to the two files
+rem set the paths to the two files
 set "file1=%~dp0..\skin.ini"
 set "file2=%~dp0skin.ini"
 
-rem Check if both files exist
+rem check if both files exist
 if not exist "%file1%" (
     echo ERROR: %file1% not found.
     exit /b 1
@@ -15,11 +15,13 @@ if not exist "%file2%" (
     exit /b 1
 )
 
-rem Swap the two files
-echo Swapping %file1% and %file2%...
+rem swap the two files
+echo swapping %file1% and %file2%...
 move /y "%file1%" "%temp%\skin.tmp" >nul
 move /y "%file2%" "%file1%" >nul
 move /y "%temp%\skin.tmp" "%file2%" >nul
 
-echo Done.
+echo files swapped, press any key to close
+pause >nul
+
 endlocal
