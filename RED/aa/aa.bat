283ec6b032333ea2e657f436f563b757b0fb1bb8
jamunano <42661264+cpuQ@users.noreply.github.com>
2024-04-08 22:42:18 +0900

    fix stuff

--- RED/aa/aa.bat
+++ RED/aa/aa.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 
 set "input_folder=ununsed"
 set "output_folder=unused_amped"
-set "amplification_db=16"
+set "amplification_db=12"
 
 for %%F in ("%input_folder%\*.wav") do (
     sox "%%F" "%output_folder%\%%~nF.wav" gain !amplification_db!
